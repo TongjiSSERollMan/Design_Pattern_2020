de0a1fbaa8cc7e1c1e49cde4a6e08003bdc5c0bd
aodethri <740950342@qq.com>
2020-11-22 17:43:28 +0800

    Business Delegate

---
+++ src/main/java/com/github/tongjisserollman/iceamusementpark/businessdelegate/DirectorsService.java
@@ -0,0 +1,13 @@
+package com.github.tongjisserollman.iceamusementpark.businessdelegate;
+
+/**
+ * @author aodethri
+ *
+ * 董事会的服务
+ */
+public interface DirectorsService {
+    /**
+     * 执行相关服务
+     */
+    void run();
+}
