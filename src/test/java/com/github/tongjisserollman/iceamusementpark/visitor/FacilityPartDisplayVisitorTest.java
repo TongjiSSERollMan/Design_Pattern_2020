87152aa12e4237055a958aa43c9160fae5839817
Xuedixuedi <958190137@qq.com>
2020-11-22 16:15:52 +0800

    add visitor

--- src/test/java/com/github/tongjisserollman/iceamusementpark/visitor/FacilityPartDisplayVisitorTest.java
+++ src/test/java/com/github/tongjisserollman/iceamusementpark/visitor/FacilityPartDisplayVisitorTest.java
@@ -3,7 +3,8 @@ package com.github.tongjisserollman.iceamusementpark.visitor;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FacilityPartDisplayVisitorTest {
-    public static void main(String[] args) {
+    @org.junit.jupiter.api.Test
+    public void FacilityPartDisplayVisitorTest() {
         Facility facility = new Facility();
         facility.accept(new FacilityPartDisplayVisitor());
     }
