26905b9196d113c4ddfd57d4aa8c50f8c0969fb0
aodethri <740950342@qq.com>
2020-11-22 18:07:21 +0800

    State

---
+++ src/main/java/com/github/tongjisserollman/iceamusementpark/state/Teacup.java
@@ -0,0 +1,32 @@
+package com.github.tongjisserollman.iceamusementpark.state;
+
+/**
+ * 旋转茶杯对象
+ */
+public class Teacup {
+
+    /**
+     * 旋转茶杯的状态
+     */
+    private State state;
+
+    public Teacup(){
+        state = null;
+    }
+
+    /**
+     * 设置旋转茶杯的状态
+     * @param state 目标状态
+     */
+    public void setState(State state){
+        this.state = state;
+    }
+
+    /**
+     * 获取旋转茶杯的状态
+     * @return 目前状态
+     */
+    public State getState(){
+        return state;
+    }
+}
