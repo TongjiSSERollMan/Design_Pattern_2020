e23ac6efc633b06910d62fa7e2ea62ef1b84d16c
aodethri <740950342@qq.com>
2020-11-24 20:22:49 +0800

    Delegate

---
+++ src/main/java/com/github/tongjisserollman/iceamusementpark/delegate/Show.java
@@ -0,0 +1,14 @@
+package com.github.tongjisserollman.iceamusementpark.delegate;
+
+/**
+ * @author aodethri
+ *
+ * 抽象任务接口
+ */
+public interface Show {
+
+    /**
+     * 由谁来表演节目
+     */
+    void startShow();
+}
