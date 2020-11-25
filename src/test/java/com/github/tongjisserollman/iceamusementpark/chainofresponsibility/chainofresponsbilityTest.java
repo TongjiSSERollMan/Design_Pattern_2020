a16a25c7f720e4708378120e841f6070e3baf918
hengqu4 <529914423@qq.com>
2020-11-25 15:20:04 +0800

    chain

--- src/test/java/com/github/tongjisserollman/iceamusementpark/chainofresponsibility/chainofresponsbilityTest.java
+++ src/test/java/com/github/tongjisserollman/iceamusementpark/chainofresponsibility/chainofresponsbilityTest.java
@@ -2,6 +2,10 @@ package com.github.tongjisserollman.iceamusementpark.chainofresponsibility;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author hq
+ */
+
 class ChainOfResponsibilityTest {
 
     @Test
