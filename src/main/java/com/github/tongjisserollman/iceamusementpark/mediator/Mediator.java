bbfb01893a64531b5bbde00cabe15c00bfff57e2
Mrcopytuo <920411694@qq.com>
2020-11-25 10:03:05 +0800

    中介者模式丰富

--- src/main/java/com/github/tongjisserollman/iceamusementpark/mediator/Mediator.java
+++ src/main/java/com/github/tongjisserollman/iceamusementpark/mediator/Mediator.java
@@ -1,28 +1,12 @@
 package com.github.tongjisserollman.iceamusementpark.mediator;
 
-import com.github.tongjisserollman.iceamusementpark.util.CallStackLogInfo;
-import com.github.tongjisserollman.iceamusementpark.util.CallStackLogger;
-
 /**
  * @author Mrcopytuo
  *
- * 中介系统
+ * 中介系统接口
  */
 
-public class Mediator {
-
-    /**
-     * 中介系统发布需求消息
-     */
-    public static void tellLack(Shop shop, String str){
-        CallStackLogger.log(
-                new CallStackLogInfo(
-                        "Mediator",
-                        "tellLack",
-                        "1145141515",
-                        "中介系统发布需求消息"
-                )
-        );
-        System.out.println("商店名："+shop.getName()+"\t需求："+str);
-    }
+public interface Mediator {
+    public void addShop(Shop shop);
+    public void tellMsg(Shop shop, String str);
 }
