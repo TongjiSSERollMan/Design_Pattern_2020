8a3cb6803f0de00d502aa65993eec9b603735a48
Mrcopytuo <920411694@qq.com>
2020-11-25 12:05:29 +0800

    前端控制器模式

---
+++ src/main/java/com/github/tongjisserollman/iceamusementpark/frontcontroller/HomeView.java
@@ -0,0 +1,19 @@
+package com.github.tongjisserollman.iceamusementpark.frontcontroller;
+
+import com.github.tongjisserollman.iceamusementpark.util.CallStackLogInfo;
+import com.github.tongjisserollman.iceamusementpark.util.CallStackLogger;
+
+/**
+ * @author Mrcopytuo
+ *
+ * 网页首页
+ */
+
+public class HomeView {
+    /**
+     * 页面展示
+     */
+    public void show(){
+        System.out.println("欢迎来到冰天雪地娱乐园！");
+    }
+}
