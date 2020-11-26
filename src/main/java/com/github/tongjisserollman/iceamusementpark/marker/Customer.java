25968fb7691a6bb261feee8529135c12d74f2b95
Mrcopytuo <920411694@qq.com>
2020-11-26 16:47:42 +0800

    marker

---
+++ src/main/java/com/github/tongjisserollman/iceamusementpark/marker/Customer.java
@@ -0,0 +1,13 @@
+package com.github.tongjisserollman.iceamusementpark.marker;
+
+/**
+ * @author Mrcopytuo
+ *
+ * 游客类
+ */
+
+public class Customer {
+    public void enterAccess(){
+        System.out.println("游客进入了员工通道");
+    }
+}
