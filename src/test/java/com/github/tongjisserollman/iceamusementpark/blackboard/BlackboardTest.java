8ca19c88e10bdb69f1828f3dddac26687e6495c0
aodethri <740950342@qq.com>
2020-11-27 10:15:44 +0800

    Blackboard

---
+++ src/test/java/com/github/tongjisserollman/iceamusementpark/blackboard/BlackboardTest.java
@@ -0,0 +1,19 @@
+package com.github.tongjisserollman.iceamusementpark.blackboard;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author aodethri
+ */
+public class BlackboardTest {
+
+    @Test
+    public void blackboardTest(){
+        Blackboard blackboard = new Blackboard();
+        Control control = new Control(blackboard);
+        control.addSource("今日门票优惠:八折");
+        control.addSource("今日旋转茶杯养护，暂不开放");
+        control.loop();
+        blackboard.inspect();
+    }
+}
