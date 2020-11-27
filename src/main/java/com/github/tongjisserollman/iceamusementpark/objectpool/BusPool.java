082add120107b9a3f50062e0da31515a45e1518f
Xuedixuedi <958190137@qq.com>
2020-11-27 18:51:11 +0800

    finish object Pool

---
+++ src/main/java/com/github/tongjisserollman/iceamusementpark/objectpool/BusPool.java
@@ -0,0 +1,77 @@
+package com.github.tongjisserollman.iceamusementpark.objectpool;
+
+import java.util.Hashtable;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author xuedixuedi
+ */
+public abstract class BusPool<T> {
+    private ConcurrentLinkedQueue<T> pool;
+    private ScheduledExecutorService executorService;
+
+    public BusPool(final int minObjects) {
+        initialize(minObjects);
+
+    }
+
+    public BusPool(final int minObjects, final int maxObjects, final long validationInterval) {
+        initialize(minObjects);
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleWithFixedDelay(new Runnable()  // annonymous class
+        {
+            @Override
+            public void run() {
+                int size = pool.size();
+
+                if (size < minObjects) {
+                    int sizeToBeAdded = minObjects + size;
+                    for (int i = 0; i < sizeToBeAdded; i++) {
+                        pool.add(createObject());
+                    }
+                } else if (size > maxObjects) {
+                    int sizeToBeRemoved = size - maxObjects;
+                    for (int i = 0; i < sizeToBeRemoved; i++) {
+                        pool.poll();
+                    }
+                }
+            }
+        }, validationInterval, validationInterval, TimeUnit.SECONDS);
+    }
+
+    public T borrowObject() {
+        T object;
+        if ((object = pool.poll()) == null) {
+            object = createObject();
+        }
+        return object;
+    }
+
+    public void returnObject(T object) {
+        if (object == null) {
+            return;
+        }
+        this.pool.offer(object);
+    }
+
+    public void shutdown() {
+        if (executorService != null) {
+            executorService.shutdown();
+        }
+    }
+
+    protected abstract T createObject();
+
+    private void initialize(final int minObjects) {
+        pool = new ConcurrentLinkedQueue<T>();
+        for (int i = 0; i < minObjects; i++) {
+            pool.add(createObject());
+        }
+    }
+
+    ;
+}
